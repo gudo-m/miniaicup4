4a723462f4a7fedb24e85e19d399fecbd258487f
Denis Mulin <gudo.m@yandex.ru>
2019-08-03 10:47:25 +0400

    not working (always left)

--- autolearner.py
+++ autolearner.py
@@ -2,24 +2,15 @@ import os
 import time
 import subprocess
 
-#
-# n_before = len(os.listdir('params/'))
-# cmd = 'python3 localrunner.py -p1 "python3 strategy.py" -p2 "python3 ../examples/python_strategy.py"  -p3 "python3 ../examples/python_strategy.py" -p4 "python3 ../examples/python_strategy.py" -p5 "python3 ../examples/python_strategy.py" -p6 "python3 ../examples/python_strategy.py" '
-# args = [f'-p{i} simple_bot' for i in range(2, 7)]
-#
-# while True:
-#     prcss = subprocess.Popen(['python3', 'localrunner.py', '-p1 "python3 strategy.py"', args[0], args[1], args[2], args[3], args[4]], stdout=subprocess.PIPE)
-#     while len(os.listdir('params/')) == n_before:
-#         pass
-#     n_before = len(os.listdir('params/'))
-#     time.sleep(1)
-#     prcss.kill()
+
+cmd = 'python3 localrunner.py -p3 "python3 strategy.py"  -p1 simple_bot -p4 simple_bot -p5 simple_bot -p6 simple_bot -p2 simple_bot --no-gui'
+cmd2 = 'python3 localrunner.py -p1 "python3 strategy.py" -p2 simple_bot -p3 simple_bot -p4 simple_bot -p5 simple_bot -p6 simple_bot --no-gui'
 
 i = 0
 while True:
     i += 1
-    os.system('python3 localrunner.py -p1 "python3 strategy.py" -p2 simple_bot -p3 simple_bot -p4 simple_bot -p5 simple_bot -p6 simple_bot --no-gui')
+    os.system(cmd)
     time.sleep(0.2)
     os.system('find . -name "*.log.gz" -exec rm -f {} \;')
-    if i % 100 == 0:
+    if i % 25 == 0:
         print(i)
--- strategy.py
+++ strategy.py
@@ -8,20 +8,22 @@ import numpy as np
 import datetime
 from multiprocessing import Process
 
-config = input()
+config = json.loads(input())
+
+flag = True
 
 
 class PolicyEstimator(nn.Module):
     def __init__(self):
         super().__init__()
-        self.n_inputs = 3
+        self.n_inputs = 964
         self.n_outputs = 4
 
         # Define network
         self.network = nn.Sequential(
-            nn.Linear(self.n_inputs, 16),
+            nn.Linear(self.n_inputs, 32),
             nn.ReLU(),
-            nn.Linear(16, self.n_outputs),
+            nn.Linear(32, self.n_outputs),
             nn.Softmax(dim=-1)
         )
 
@@ -39,19 +41,62 @@ def discount_rewards(lrewards, gamma=0.99):
     return r - r.mean()
 
 
+def make_normal_coord(coord):
+    return coord // config['params']['width']
+
+
+def edit_cells_by_player(cells, player):
+    for my_cell in player['territory']:
+        x = make_normal_coord(my_cell[0])
+        y = make_normal_coord(my_cell[1])
+        cells[x * y] = 2
+    if player.get('lines', False):
+        for line in player['lines']:
+            x = make_normal_coord(line[0])
+            y = make_normal_coord(line[1])
+            cells[x * y] = 3
+    x = make_normal_coord(player['position'][0])
+    y = make_normal_coord(player['position'][1])
+    cells[x * y] = -1
+    return cells
+
+
 def make_ls(lstate):
-    with open('state', 'w') as f:
-        f.write(str(lstate))
-        f.write('\n'+str(config))
+
     commands_int = {'left': 0, 'right': 1, 'up': 2, 'down': 3}
     ls = lstate['params']['players']['i']
+
     direction = -1 if ls['direction'] is None else commands_int[ls['direction']]
+    cells = [1 for _ in range(31*31)]
+
+    for my_cell in ls['territory']:
+        x = make_normal_coord(my_cell[0])
+        y = make_normal_coord(my_cell[1])
+        cells[x * y] = 0
+    if ls.get('lines', False):
+        for line in ls['lines']:
+            x = make_normal_coord(line[0])
+            y = make_normal_coord(line[1])
+            cells[x * y] = -2
+    x = make_normal_coord(ls['position'][0])
+    y = make_normal_coord(ls['position'][1])
+    cells[x * y] = -1
+
     learning_state = {
         'direction': int(direction),
         'pos_x': int(ls['position'][0]),
-        'pos_y': int(ls['position'][1])
+        'pos_y': int(ls['position'][1]),
     }
-    reward = ls['score'] - score
+
+    for cell_i in range(len(cells)):
+        learning_state[f'cell{cell_i}'] = cells[cell_i]
+
+    reward = ls['score'] - (score)
+
+    with open('state', 'w') as f:
+        f.write(str(lstate))
+        f.write('\n'+str(config))
+        f.write('\n'+str(learning_state))
     return learning_state, reward
 
 
@@ -122,6 +167,12 @@ while True:
                 ]
             )
 
+        if np.mean(total_rewards[-10:]) > 0 and flag:
+            torch.save(pe.state_dict(), PATH + f'(больше 0)-{datetime.datetime.now()}')
+            flag = False
+        elif not flag:
+            flag = True
+
         break
 
     ls, reward = make_ls(state)
@@ -139,5 +190,5 @@ while True:
 
 torch.save(pe.state_dict(), PATH)
 
-if datetime.datetime.now().second % 15 == 0:
+if datetime.datetime.now().second % 15 in (0, 5, 3):
     torch.save(pe.state_dict(), PATH+f'-{datetime.datetime.now()}')
