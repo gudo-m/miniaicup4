f5adf57bfa729ed948fe1c75e8b50d07413c9af7
Denis Mulin <gudo.m@yandex.ru>
2019-08-03 22:19:11 +0400

    very good version, learning

--- autolearner.py
+++ autolearner.py
@@ -5,36 +5,56 @@ import numpy as np
 import torch
 from torch import nn
 import torch.optim as optim
-import torch.nn.functional as F
 import datetime
 import random
+from collections import OrderedDict
+gui = False
 
 
 class Net(nn.Module):
-    def __init__(self, obs_size, hidden_size, n_actions):
+    def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(obs_size, hidden_size)
-        self.fc2 = nn.Linear(hidden_size, n_actions)
+        self.convnet = nn.Sequential(OrderedDict([
+            ('c1', nn.Conv2d(1, 6, kernel_size=(4, 4))),
+            ('relu1', nn.ReLU()),
+            ('s2', nn.MaxPool2d(kernel_size=(2, 2), stride=2)),
+            ('c3', nn.Conv2d(6, 16, kernel_size=(4, 4))),
+            ('relu3', nn.ReLU()),
+            ('s4', nn.MaxPool2d(kernel_size=(2, 2), stride=2)),
+            ('c5', nn.Conv2d(16, 120, kernel_size=(4, 4))),
+            ('relu5', nn.ReLU())
+        ]))
 
-    def forward(self, x):
-        x = F.relu(self.fc1(x))
-        return self.fc2(x)
+        self.fc = nn.Sequential(OrderedDict([
+            ('f6', nn.Linear(480, 84)),
+            ('relu6', nn.ReLU()),
+            ('f7', nn.Linear(84, 4)),
+        ]))
+
+    def forward(self, img):
+        output = self.convnet(img)
+        output = output.view(img.size(0), -1)
+        output = self.fc(output)
+        return output
 
 
-cmd = 'python3 localrunner.py -p3 "python3 strat.py"  -p1 simple_bot -p4 simple_bot -p5 simple_bot -p6 simple_bot -p2 simple_bot --no-gui'
-cmd1 = 'python3 localrunner.py -p4 "python3 strat.py"  -p1 simple_bot -p3 simple_bot -p5 simple_bot -p6 simple_bot -p2 simple_bot --no-gui'
-cmd3 = 'python3 localrunner.py -p5 "python3 strat.py"  -p1 simple_bot -p3 simple_bot -p4 simple_bot -p6 simple_bot -p2 simple_bot --no-gui'
-cmd4 = 'python3 localrunner.py -p6 "python3 strat.py"  -p1 simple_bot -p3 simple_bot -p4 simple_bot -p5 simple_bot -p2 simple_bot --no-gui'
-cmd5 = 'python3 localrunner.py -p2 "python3 strat.py"  -p1 simple_bot -p4 simple_bot -p5 simple_bot -p6 simple_bot -p3 simple_bot --no-gui'
-cmd2 = 'python3 localrunner.py -p1 "python3 strat.py" -p2 simple_bot -p3 simple_bot -p4 simple_bot -p5 simple_bot -p6 simple_bot --no-gui'
-
+cmd = 'python3 localrunner.py -p3 "python3 strat.py"  -p1 simple_bot -p4 simple_bot -p5 simple_bot -p6 simple_bot -p2 simple_bot'
+cmd1 = 'python3 localrunner.py -p4 "python3 strat.py"  -p1 simple_bot -p3 simple_bot -p5 simple_bot -p6 simple_bot -p2 simple_bot'
+cmd3 = 'python3 localrunner.py -p5 "python3 strat.py"  -p1 simple_bot -p3 simple_bot -p4 simple_bot -p6 simple_bot -p2 simple_bot'
+cmd4 = 'python3 localrunner.py -p6 "python3 strat.py"  -p1 simple_bot -p3 simple_bot -p4 simple_bot -p5 simple_bot -p2 simple_bot'
+cmd5 = 'python3 localrunner.py -p2 "python3 strat.py"  -p1 simple_bot -p4 simple_bot -p5 simple_bot -p6 simple_bot -p3 simple_bot'
+cmd2 = 'python3 localrunner.py -p1 "python3 strat.py" -p2 simple_bot -p3 simple_bot -p4 simple_bot -p5 simple_bot -p6 simple_bot'
+cmds = [cmd2, cmd5, cmd, cmd1, cmd3, cmd4]
 
 def generate_batch(batch_size):
     print(f'generating batch...')
     batch_actions, batch_states, batch_rewards = [], [], []
 
     for b in range(batch_size):
-        os.system(random.choice([cmd, cmd1, cmd2, cmd3, cmd4, cmd5]))
+        choosen = random.choice(cmds)
+        if gui:
+            print(cmds.index(choosen)+1)
+        os.system(choosen if gui else choosen+' --no-gui')
         time.sleep(0.1)
         os.system('find . -type f -name "*.log.gz" -exec rm -f {} \;')
         with open('log', 'r') as f:
@@ -43,6 +63,7 @@ def generate_batch(batch_size):
             line = json.loads(f.readline().strip())
             batch_actions.append(line)
             line = json.loads(f.readline().strip())
+            print(f'{line} - reward')
             batch_rewards.append(line)
         print(f'{b+1}/{batch_size} of batch ended')
     return batch_states, batch_actions, batch_rewards
@@ -63,18 +84,18 @@ def filter_batch(states_batch, actions_batch, rewards_batch, percentile=50):
 
 
 if __name__ == '__main__':
-    batch_size = 50
+    batch_size = 100
     session_size = 100
-    percentile = 90
-    hidden_size = 1024
+    percentile = 70
+    hidden_size = 200
     learning_rate = 0.0025
     completion_score = 200
 
-    n_states = 963
+    n_states = 961
     n_actions = 4
 
     # neural network
-    net = Net(n_states, hidden_size, n_actions)
+    net = Net()
     # load params
     try:
         net.load_state_dict(torch.load('params/param19'))
@@ -92,8 +113,10 @@ if __name__ == '__main__':
         batch_states, batch_actions, batch_rewards = generate_batch(batch_size)
         elite_states, elite_actions = filter_batch(batch_states, batch_actions, batch_rewards, percentile)
         optimizer.zero_grad()
-        tensor_states = torch.FloatTensor(elite_states)
+
+        tensor_states = torch.FloatTensor(np.array(elite_states).reshape([len(elite_states), 1, 31, 31]))
         tensor_actions = torch.LongTensor(elite_actions)
+
         action_scores_v = net(tensor_states)
         loss_v = objective(action_scores_v, tensor_actions)
         loss_v.backward()
--- strat.py
+++ strat.py
@@ -1,21 +1,15 @@
 import json
-import random
 import numpy as np
 import torch
 from torch import nn
-import torch.optim as optim
-import torch.nn.functional as F
 from autolearner import Net
 config = json.loads(input())
 score = 0
 
 
 def make_first_ls(ls):
-    learning_state = {
-        'pos_x': ls['pos_x'],
-        'pos_y': ls['pos_y'],
-    }
-    cells = [1 for _ in range(31*31)]
+    learning_state = {}
+    cells = [0.2 for _ in range(31*31)]
     for cell_i in range(len(cells)):
         learning_state[f'cell{cell_i}'] = cells[cell_i]
     return learning_state
@@ -29,51 +23,54 @@ def edit_cells_by_player(cells, player):
     for my_cell in player['territory']:
         x = make_normal_coord(my_cell[0])
         y = make_normal_coord(my_cell[1])
-        cells[x * y] = 2
+        cells[x * y] = 1.5
     if player.get('lines', False):
         for line in player['lines']:
             x = make_normal_coord(line[0])
             y = make_normal_coord(line[1])
-            cells[x * y] = 3
+            cells[x * y] = 2
     x = make_normal_coord(player['position'][0])
     y = make_normal_coord(player['position'][1])
-    cells[x * y] = -1
+    cells[x * y] = -2
     return cells
 
 
 def make_ls(lstate):
+    global score
     commands_int = {'left': 0, 'right': 1, 'up': 2, 'down': 3}
     ls = lstate['params']['players']['i']
 
-    direction = -1 if ls['direction'] is None else commands_int[ls['direction']]
-    cells = [1 for _ in range(31*31)]
+    cells = [0.5 for _ in range(31*31)]
 
     for my_cell in ls['territory']:
         x = make_normal_coord(my_cell[0])
         y = make_normal_coord(my_cell[1])
-        cells[x * y] = 0
+        cells[x * y] = 1.25
     if ls.get('lines', False):
         for line in ls['lines']:
             x = make_normal_coord(line[0])
             y = make_normal_coord(line[1])
-            cells[x * y] = -2
+            cells[x * y] = -0.5
     x = make_normal_coord(ls['position'][0])
     y = make_normal_coord(ls['position'][1])
-    cells[x * y] = -1
+    cells[x * y] = 0
 
-    learning_state = {
-        'pos_x': ls['position'][0]/1000,
-        'pos_y': ls['position'][1]/1000,
-    }
+    for pl in lstate['params']['players'].keys():
+        if pl == 'i':
+            continue
+        cells = edit_cells_by_player(cells, lstate['params']['players'][pl])
+
+    learning_state = {}
 
     for cell_i in range(len(cells)):
         learning_state[f'cell{cell_i}'] = cells[cell_i]
 
     reward = ls['score'] - (score)
+    score = ls['score']
     return learning_state, reward
 
 
-model = Net(963, 1024, 4)
+model = Net()
 try:
     model.load_state_dict(torch.load('params/param19'))
     model.eval()
@@ -99,9 +96,9 @@ tick = 1
 
 while True:
     try:
-        new_s = json.loads(input())
-        tick = new_s.get('params', {}).get('tick_num', 1)
-        new_s, r = make_ls(new_s)
+        main_s = json.loads(input())
+        tick = main_s.get('params', {}).get('tick_num', 1)
+        new_s, r = make_ls(main_s)
         new_s = new_s
     except:
         break
@@ -112,9 +109,11 @@ while True:
 
     commands = ['left', 'right', 'up', 'down']
 
-    s_v = torch.FloatTensor(s)
-    act_probs_v = activation(model(s_v))
+    s_v = torch.FloatTensor(np.array(new_s).reshape([1, 1, 31, 31]))
+    act_probs_v = activation(model(s_v)[0])
     act_probs = act_probs_v.data.numpy()
+    with open('vals', 'w') as f:
+        f.write(str(act_probs_v)+'\n'+str(act_probs))
     a = np.random.choice(commands, p=act_probs_v.detach().numpy())
     states.append(s)
     actions.append(commands.index(a))
@@ -122,10 +121,14 @@ while True:
 
     s = new_s
 
+    pos = main_s['params']['players']['i']['position']
+
     print(json.dumps({"command": a, 'debug': str(new_s)}))
 
 if tick == 0:
     tick = 1
+if tick < 50:
+    total_reward = -300
 total_reward = total_reward - (20/tick)
 with open('log', 'w') as f:
     f.write(
